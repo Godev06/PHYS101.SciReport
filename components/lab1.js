720c8850944ea4afe7e7ee1d3c7e7bc817985a62
Nguyentrongtien <tien.nguyentrong.cit24@eiu.edu.vn>
2026-04-21 08:24:15 +0700

    fix: enhance formatting for measurement accuracy display in lab report

--- components/lab1.js
+++ components/lab1.js
@@ -44,9 +44,9 @@ const TASK_METHOD_HTML = `
     <div class="space-y-1 text-xs md:text-base leading-relaxed">
       <p>Nhiệm vụ: đo các hằng số cơ bản: chiều dài, khối lượng, thời gian.</p>
       <p>Phương pháp đo:</p>
-      <p class="pl-4">Độ chính xác của thước kẹp: CCX = 0.05 (mm).</p>
+      <p class="pl-4">Độ chính xác của thước kẹp: <b>CCX = 0.05 (mm)</b>.</p>
       <p class="pl-4">Độ biến thiên khối lượng hệ thống của khối nhôm là: \\( \\Delta m_{ht} = 0.01\\,g \\).</p>
-      <p class="pl-4">Độ chính xác của thước panme: CCX = 0.01 (mm).</p>
+      <p class="pl-4">Độ chính xác của thước panme: <b>CCX = 0.01 (mm)</b>.</p>
       <p class="pl-4">Độ biến thiên khối lượng hệ thống của khối đồng là: \\( \\Delta m_{ht} = 0.01\\,g \\).</p>
     </div>
   </div>
